f8923f2bcd90d5e39e21a36c8c7ebf49f053102b
han18 <han.fadah@gmail.com>
2024-09-23 11:24:23 -0400

    compeleted first part

--- main.js
+++ main.js
@@ -7,20 +7,21 @@ const plants = 20;
 // starting with 20 plants Each plant requires a minimum space of 0.8 square meters.
 const sqareSpace = 0.8
 
-// area and the space of the plants
+// area and the space of the plants Max capacity
 const plantSpace = Math.round(area / sqareSpace) ;
 console.log(plantSpace);
+
 
 const plantSqare = Math.round(area / plants) 
 console.log(plantSqare)
 
 
 // the precentage of growth
-const eightPer = 0.80
-const fiftyPer = 0.50 
+const eightPer = 0.80 * plantSpace;
+const fiftyPer = 0.50  * plantSpace;
 
 // week one
-if(plantSqare > eightPer * plants ) {
+if(plantSqare > eightPer ) {
     console.log("Pruned")
 } else if (plantSqare > fiftyPer && plantSqare < eightPer ) {
     console.log("Monitored")
@@ -30,13 +31,19 @@ if(plantSqare > eightPer * plants ) {
     console.log("Do not plant in the area!")
 }
 
-// // week one
-// if(plantSpace > eightPer  * plants ) {
-//     console.log("Pruned")
-// } else if (plantSpace > fiftyPer && plantSpace < eightPer ) {
-//     console.log("Monitored")
-// } else if (plantSpace < fiftyPer) {
-//     console.log("Planted")
-// } else {
-//     console.log("Do not plant in the area!")
-// }
+const plantDouble = plantSqare * 2;
+
+// week one
+if(plantDouble > eightPer ) {
+    console.log("Pruned")
+} else if (plantDouble > fiftyPer && plantDouble < eightPer ) {
+    console.log("Monitored")
+} else if (plantDouble < fiftyPer) {
+    console.log("Planted")
+} else {
+    console.log("Do not plant in the area!")
+}
+
+
+// Part 2: Thinking Bigger
+
